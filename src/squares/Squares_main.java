a73e14b1575be8f28ecc40382c3e49ac4381c69d
50616029 <50616029@50616029-pc>
2018-01-22 17:41:11 +0900

    1/22 csv 50616029 坪田颯生

--- src/squares/Squares_main.java
+++ src/squares/Squares_main.java
@@ -1,14 +1,24 @@
 package squares;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Squares_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double x[] = { 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2 };
-		double y[] = { 1.0, 1.5, 2.2, 5.3, 4.8, 7.2, 8.2 };
+		//double x[] = { 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2 };
+		Squares_main smain = new Squares_main();
+		double x[] = smain.getCSV1("csv/x.csv",7);
+		//double y[] = { 1.0, 1.5, 2.2, 5.3, 4.8, 7.2, 8.2 };
+		double y[] = smain.getCSV1("csv/y.csv",7);
 		double theta[] = new double[2];
 
 		Squares_lib slib = new Squares_lib(x, y);
+		double value[] = new double[100];
 		
 		for(int i = 0; i < 100; i++){
 			 theta = slib.getDx();
@@ -17,7 +27,37 @@ public class Squares_main {
 			 System.out.println(i+" : Objective function = "+slib.getObject());
         }
 
-
+		Graph graph = new Graph(value);
+		graph.setBounds(5,5,655,455);
+		graph.setVisible(true);
 	}
+	public double[] getCSV1(String path, int n) { //CSVから1次元データ取り込み(n:データ数)
+        //CSVから取り込み
+        double csvdata[] = new double[n];
+        try {
+            File f = new File(path);
+            BufferedReader br = new BufferedReader(new FileReader(f));
+                 
+            String[] data = new String[n];
+            String line = br.readLine();
+            data = line.split(",", 0);
+            line = br.readLine();
+            br.close();
+            
+            // CSVから読み込んだ配列の中身を取り込み
+            for(int i = 0; i < data.length; i++) {
+                csvdata[i] = Double.parseDouble(data[i]);
+            }
 
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println("CSV取り込み = "+Arrays.toString(csvdata));
+        return csvdata;
+        //CSVから取り込みここまで
+    }
+
+	
 }
+
+
